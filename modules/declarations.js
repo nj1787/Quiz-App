9a7ab842eb03526041894787e38bd34ed5b48ab9
nj1787 <neerajj9@gmail.com>
2024-01-24 23:50:02 +0530

    Removed Modules Due To Errors.

--- modules/declarations.js
+++
@@ -1,7 +0,0 @@
-export const questionDisplay = document.getElementById("question-display");
-
-export const optionsDisplay = document.getElementById("options-display");
-
-export const scoreDisplay = document.getElementById("score-display");
-
-export const nextQuesBtn = document.getElementById("next");
--- script.js
+++ script.js
@@ -1,17 +1,97 @@
-import {
-  questionDisplay,
-  optionsDisplay,
-  scoreDisplay,
-} from "./modules/declarations.js";
+let currentQuestionIndex = 1;
 
-import {
-  questionElement,
-  optionsListElement,
-  scoreElement,
-} from "./modules/elements.js";
+let currentScore = 0;
 
-questionDisplay.append(questionElement);
+const totalScore = 5;
 
-optionsDisplay.append(optionsListElement);
+const questionObjects = [
+  {
+    questionId: 1,
+    question: "Which of the following is capital of India ?",
+    options: ["New Delhi", "Mumbai", "Chennai", "Kolkata"],
+    answer: "New Delhi",
+  },
+  {
+    questionId: 2,
+    question: "Who is the Prime Minister Of India ?",
+    options: ["Rajnath Singh", "Amit Shah", "S. Jaishankar", "Narendra Modi"],
+    answer: "Narendra Modi",
+  },
+];
 
-scoreDisplay.append(scoreElement);
+const questionDisplay = document.getElementById("question-display");
+
+const optionsDisplay = document.getElementById("options-display");
+
+const displayNextBtn = document.getElementById("next");
+
+const scoreDisplay = document.getElementById("score-display");
+
+const currentQuestion = questionObjects.filter(
+  (q) => q.questionId === currentQuestionIndex
+);
+
+const { question, options } = currentQuestion[0];
+
+const h3 = document.createElement("h3");
+h3.innerHTML = question;
+questionDisplay.append(h3);
+const ul = document.createElement("ul");
+options.forEach((o) => {
+  const li = document.createElement("li");
+  li.setAttribute("class", "option");
+  li.addEventListener("click", checkAnswer);
+  li.innerHTML = o;
+  ul.append(li);
+});
+optionsDisplay.append(ul);
+
+const score = document.createElement("h3");
+score.setAttribute("id", "current-score");
+score.innerHTML = `${currentScore} / ${totalScore}`;
+scoreDisplay.append(score);
+
+displayNextBtn.addEventListener("click", nextQuestion);
+
+// Functionalities
+
+function nextQuestion() {
+  currentQuestionIndex += 1;
+  questionDisplay.innerHTML = "";
+  optionsDisplay.innerHTML = "";
+  const currentQuestion = questionObjects.filter(
+    (q) => q.questionId === currentQuestionIndex
+  );
+
+  const { question, options } = currentQuestion[0];
+
+  const h3 = document.createElement("h3");
+  h3.innerHTML = question;
+  questionDisplay.append(h3);
+  const ul = document.createElement("ul");
+  options.forEach((o) => {
+    const li = document.createElement("li");
+    li.setAttribute("class", "option");
+    li.addEventListener("click", checkAnswer);
+    li.innerHTML = o;
+    ul.append(li);
+  });
+  optionsDisplay.append(ul);
+}
+
+function checkAnswer(obj) {
+  const selectedAnswer = obj.target.innerHTML;
+  const currentQuestion = questionObjects.filter(
+    (q) => q.questionId === currentQuestionIndex
+  );
+
+  const { answer } = currentQuestion[0];
+  if (selectedAnswer === answer) {
+    currentScore += 1;
+    optionsDisplay.innerHTML = "";
+  } else {
+    currentScore -= 1;
+    optionsDisplay.innerHTML = "";
+  }
+  score.innerHTML = `${currentScore} / ${totalScore}`;
+}
